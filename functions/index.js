a83ec13bc8859915f5bbfeee7cc12cc5187a4c14
rodchananat <rod8711@gmail.com>
2018-07-01 22:54:43 +0700

    init Person when imported (from DTNL?)

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
-// Set the configuration for your app
-// TODO: Replace with your project's config object
 var firebase = require("firebase");
+var functions = require('firebase-functions');
+var admin = require('firebase-admin'); // not needed anymore?
+// admin.initializeApp();
+var bcrypt = require('bcrypt');
 var config = {
     apiKey: "AIzaSyDtoVaA50nTikqo62rNzn6lAquMJlpjvCA",
     authDomain: "rnkm2018-house-ad25ge.firebaseapp.com",
@@ -8,26 +10,8 @@ var config = {
     storageBucket: "rnkm2018-house-ad25ge.appspot.com"
 };
 firebase.initializeApp(config);
-
-
-// Get a reference to the database service
-
-var functions = require('firebase-functions');
-var admin = require('firebase-admin');
-// admin.initializeApp();
 var db = firebase.database()
 
-// var db = admin.database();
-
-
-// exports.addPerson = functions.https.onRequest((req, res) => {
-//     const id = req.query.id;
-//     const house = req.query.house;
-//     return db.ref('/person/' + id).set({ id: id, house: '4House'}).then((snapshot) => {
-//         return res.send('add person' + id);
-//     });
-
-// });
 
 exports.getHouses = functions.https.onRequest((req, res) => {
     return db.ref('/houses').once('value').then(snapshot => {
@@ -36,11 +20,13 @@ exports.getHouses = functions.https.onRequest((req, res) => {
 });
 
 exports.addPersonToHouse = functions.https.onRequest((req, res) => {
+    // need some identification ? 
+    // id should be ID number(13 digit)?
+
     var newHouse = req.query.house.trim();
     var id = req.query.id.trim();
-    // var db = admin.database();
     console.log(`req: ID:${id} house:${newHouse}`);
-    return db.ref('/houses/' + newHouse).transaction( (house) => {
+    return db.ref('/houses/' + newHouse).transaction((house) => {
         console.log('new house:', house);
         if (house === null){
             return null;
@@ -75,15 +61,50 @@ exports.addPersonToHouse = functions.https.onRequest((req, res) => {
 });
 
 
-
-exports.login = functions.https.onRequest((req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-    return db.ref('/secure/' + id).once('value').then((snapshot) => {
-        var usr = snapshot.val().username;
-        var pwHash = snapshot.val().hash;
-
-
+exports.initPerson = functions.database.ref('/secure/{userId}').onCreate((snapshot, context) => {
+    // when import data from DTNL (house results + other infos ) create db in /person 
+    // and hash Their password 
+    // if password sent is already hashed I will remove hashing 
+    var user = snapshot.val();
+    var id = user.idNumber;
+    var house = user.house; 
+    var password = user.password;
+    return db.ref('/person/' + id).set({house: house}).then(() => {
+        return bcrypt.hash(password, 8, (err, hash) => {
+            if (err){
+                console.log("error hashing", err);
+                return snapshot.ref.update({hashed: false});
+            }
+            else {
+                return snapshot.ref.update({hashed: true, password: hash});
+            }
+        });
     });
+});
+
+// send cookie ?? (IDK about security OMEGALUL)
+exports.login = functions.https.onRequest((req, res) => {
+    var id = req.body.id;
+    var tel = req.body.tel;
+    var password = req.body.password;
+    // return res.send(id + '--' +tel);
+    return db.ref('/secure/' + id).once('value').then((snapshot) => {
+        var user = snapshot.val();
+        if (id == user.id && tel == user.tel){
+            return bcrypt.compare(password, user.password, (err, same) => {
+                if (err){
+                    console.log('error logging in', err);
+                    return res.send('');
+                }
+                else{
+
+                }
+            });
+        } 
+        else {
+            return res.send('');
+        }
+    });
+   
 
 });
