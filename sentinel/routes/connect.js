9ddec8a772a67033637b8ad4ffb3a23931baa640
rodchananat <rod8711@gmail.com>
2018-07-31 22:23:04 +0700

    upload sentinel

---
+++ sentinel/routes/connect.js
@@ -0,0 +1,25 @@
+var config = require('../config');
+var util = require('./util');
+
+
+query = async function (agent, house) {
+    try {
+        var dtnlData = await agent.post(`http://${config.dtnlADDR}/api/v1/get/data/${config.rnkmTablename}/1`)
+            .send({
+                sortby: "",
+                orderby: "",
+                filter: JSON.stringify([{
+                    column_name: config.houseColumn,
+                    expression: 'like',
+                    value: `^${house}$`
+                }])
+            })
+        return Promise.resolve(dtnlData.body.count);
+    }
+    catch (err) {
+        return Promise.reject(err);
+    }
+}
+module.exports = {
+    query: query
+}
