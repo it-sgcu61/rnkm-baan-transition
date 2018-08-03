9f43facba93a72840ed5b1e6ada36bbcf169fdc3
rodchananat <rod8711@gmail.com>
2018-08-03 11:56:05 +0700

    add some loadtesting scripts

---
+++ loadtest/randomize.js
@@ -0,0 +1,25 @@
+var houses = ["a-aum","abnormal","achuan","agape","buchayun","buem","dork","duidui","dung",
+"fyo","indiana","indy","jadson","jo","jodeh","khunnoo","kids","koh","koom",
+"laijai","mheenoi","nhai","pak-tak-agard","phee","por","preaw","rang","rhoy",
+"seiyw","sod","soeitee","tem","wang","wanted","work","yim"];
+var n = houses.length;
+var randInt = x => Math.floor(Math.random()*x);
+var util = require('../utils/util');
+
+randomHouse = async function(requestParams, context, ee, next) {
+    context.vars['house'] = houses[randInt(n)];
+    var {id, token} = context.vars;
+    requestParams = context.vars;
+    await util.sleep(randInt(3000));
+    return next();
+}
+logRes = function (requestParams, response, context, ee, next) {
+    var {id, tel, house} = requestParams;
+    console.log(`${id} moving to ${house} is ${response.success}`);
+    return next();
+}
+
+module.exports = {
+    randomHouse: randomHouse,
+    logRes: logRes
+}
